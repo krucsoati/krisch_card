336baefcc2055859da0094dbd0ada57d1a34bffa
Krucsó Attila <krucsoati@freemail.hu>
2013-05-29 11:55:46 +0200

    the second

--- src/Acme/DemoBundle/Controller/DemoController.php
+++ src/Acme/DemoBundle/Controller/DemoController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Acme\DemoBundle\Form\ContactType;
 
-// these import the "@Route" and "@Template" annotations
+// these import the "@Route" and "@Template" annotations ######
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
