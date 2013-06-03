3be39ad58c3f3c65c88b608ea134c0b610de4458
Krucsó Attila <krucsoati@freemail.hu>
2013-06-03 21:18:34 +0200

    főoldal elkezdése

--- src/KrischCard/WorkshopBundle/Controller/PageController.php
+++ src/KrischCard/WorkshopBundle/Controller/PageController.php
@@ -4,10 +4,13 @@ namespace KrischCard\WorkshopBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class PageController extends Controller
-{
-    public function indexAction()
-    {
-        return $this->render('KrischCardWorkshopBundle:Page:layout.html.twig');
-    }
+class PageController extends Controller {
+    
+    public function indexAction()       { return $this->render('KrischCardWorkshopBundle:Page:layout.html.twig'); }
+    
+    public function contactAction()     { return $this->render('KrischCardWorkshopBundle:Page:contact.html.twig'); }
+                        
+    public function descriptionAction() { return $this->render('KrischCardWorkshopBundle:Page:description.html.twig'); }
+    
+    public function servicesAction()    { return $this->render('KrischCardWorkshopBundle:Page:services.html.twig'); }
 }
